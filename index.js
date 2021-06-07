4704b5be3861447e0823fc07051f88e09fc27339
ulrulr <64479953+ulrulr@users.noreply.github.com>
2021-06-07 09:52:25 +0200

    feat(questions): save question 7 to 10

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ const earlywork = {
 
 // Q7. Return a new array from `debaters` with all item in uppercase
 const debaters = ['we do', 'what we have to do', 'in order to do', 'what we want to do'];
+debaters.forEach(element => console.log(element.toUpperCase()))
 
 // Q8. Using this array.
 // Do the following 5 steps
